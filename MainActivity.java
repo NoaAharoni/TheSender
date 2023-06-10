b4376d785193fe2a5247f5370ad5fc65252d3de2
NoaAharoni <117601300+NoaAharoni@users.noreply.github.com>
2023-06-10 04:07:26 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.thesender;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button toLogin, toSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toLogin = findViewById(R.id.to_login);
+        toSignUp = findViewById(R.id.to_signup);
+
+        toLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        toSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
